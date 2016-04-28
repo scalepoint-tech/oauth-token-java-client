4436bbe2de90dbd18ce7791a523ac67c5a598d09
Yuriy Ostapenko <uncleyo@users.noreply.github.com>
2016-04-28 17:23:58 +0300

    Fix doc

--- src/main/java/com/scalepoint/oauth_token_client/DelegateAccessGrantTokenClient.java
+++ src/main/java/com/scalepoint/oauth_token_client/DelegateAccessGrantTokenClient.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * OAuth2 Token endpoint client for urn:scalepoint:params:oauth:grant-type:delegate-access grant with "private_key_jwt" client authentication scheme.
+ * OAuth2 Token endpoint client for urn:scalepoint:params:oauth:grant-type:delegate-access grant.
  * Tokens are cached in-memory by default.
- *
- * @see <a href="https://tools.ietf.org/html/rfc7521#section-6.2">Assertion Framework for OAuth 2.0 Client Authentication and Authorization Grants</a>
- * @see <a href="https://openid.net/specs/openid-connect-core-1_0.html#ClientAuthentication">OpenID Connect Core 1.0</a>
  */
 public class DelegateAccessGrantTokenClient extends CustomGrantTokenClient {
     private final ClientCredentials clientCredentials;
