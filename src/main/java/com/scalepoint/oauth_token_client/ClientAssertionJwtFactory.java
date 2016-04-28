052ba0154f07f446861c93c5e133cc75807003e9
Yuriy Ostapenko <uncleyo@users.noreply.github.com>
2016-04-28 14:37:50 +0300

    Add SHA-1 thumbprint as "kid" too

--- src/main/java/com/scalepoint/oauth_token_client/ClientAssertionJwtFactory.java
+++ src/main/java/com/scalepoint/oauth_token_client/ClientAssertionJwtFactory.java
@@ -23,11 +23,13 @@ class ClientAssertionJwtFactory {
 
     public String CreateAssertionToken() {
         Date now = new Date();
+        // no need to have a long-lived token (clock skew should be accounted for on the server-side)
         Date expires = new Date(now.getTime() + 10000 /* 10 seconds */);
 
         return Jwts.builder()
                 .setHeaderParam("typ", "JWT")
                 .setHeaderParam(JwsHeader.X509_CERT_SHA1_THUMBPRINT, thumbprint)
+                .setHeaderParam(JwsHeader.KEY_ID, thumbprint)
                 .setIssuer(clientId)
                 .setSubject(clientId)
                 .setAudience(tokenEndpointUri)
